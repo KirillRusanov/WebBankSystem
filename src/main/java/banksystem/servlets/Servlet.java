4d531f9572c9f32b694e0ad862dd704a1cd86e79
Kirill Rusanov <kr.rusanov@gmail.com>
2020-11-09 09:53:47 +0300

    Add Tomcat,add WEB EE. Create first Servlet

---
+++ src/main/java/banksystem/servlets/Servlet.java
@@ -0,0 +1,24 @@
+package banksystem.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet("/test")
+public class Servlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter pw = response.getWriter();
+        pw.println("<html>");
+        pw.println("<h1> Hello world! </h1>");
+        pw.println("</html>");
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Test</title>
+  </head>
+  <body>
+  <h1>Hello world test</h1>
+  </body>
+</html>
